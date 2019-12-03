fcfb75a824a0d288e3b0c8c70463569ba3692855
Caryn McCarthy <cmccarthy15@gmail.com>
2019-12-03 09:46:53 -0500

    syntax sugar update

--- 32-es6-syntax-sugar/index.js
+++ 32-es6-syntax-sugar/index.js
@@ -1,8 +1,52 @@
-/*****         Key Value Assignment Shortcut */
-/*****         Forms of Arrow Functions */
-/*****         Spread Operator */
-/*****         Destructuring Assignment */
-/*****         Rest Operator */
-/*****         Ternary Operator */
+/*****         Key Value Assignment Shortcut 
+ *                  variables & keys matching
+ * 
+ */
+
+
+
+
+/*****         Forms of Arrow Functions 
+ *               implicit vs explicit return 
+ * 
+ */
+
+
+
+
+
+/*****         Spread Operator 
+ *                used with any iterable 
+ *                copying values (not reference)
+ *                blasting strings (so you can make a tree!) 
+ *                joining teams 🥰
+ */
+
+
+
+
+
+/*****         Destructuring Assignment 
+ *                 arrays (address example)
+ *                 objects
+ * 
+*/
+
+
+
+
+
+/*****         Rest Operator 
+ *                 examples...
+ *                 object from array
+*/
+
+
+
+
+
+/*****         Ternary Operator 
+ *                 SOMETIMES helpful
+*/
 
 
